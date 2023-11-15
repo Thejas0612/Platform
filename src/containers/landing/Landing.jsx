040e89ec20bf9e1711bb22f11f6812bcf65d7ce6
Ganesh Mallappa Uchagaonkar <ganesh.uchagaonkar@slkgroup.com>
2023-11-15 09:52:26 +0530

    Buttons were added from ui library

--- src/containers/landing/Landing.jsx
+++ src/containers/landing/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useOktaAuth } from "@okta/okta-react";
-
+import Button from "../../components/common/ButtonInput";
 export default function Landing() {
   const { t, i18n } = useTranslation();
   const { authState, oktaAuth } = useOktaAuth();
@@ -10,9 +10,11 @@ export default function Landing() {
     <div>
       {authState && authState.isAuthenticated && <h1>Click on logout for stop the session</h1>}
       {authState && !authState.isAuthenticated && <h1>Click on login to visit website</h1>}
-      <h2>{t("Welcome")}</h2>
-      <button onClick={() => i18n.changeLanguage("en")}>English</button>
-      <button onClick={() => i18n.changeLanguage("fr")}>French</button>
+      <h1>{t("Welcome")}</h1>
+      <div style={{ display: "flex", justifyContent: "start", alignItems: "center", gap: "10px" }}>
+        <Button label="English" onClick={() => i18n.changeLanguage("en")} />
+        <Button label="French" onClick={() => i18n.changeLanguage("fr")} />
+      </div>
     </div>
   );
 }
