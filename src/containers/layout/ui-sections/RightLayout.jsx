af58178924e0035390fc51febd0b0a4b4b872c22
Ganesh Mallappa Uchagaonkar <ganesh.uchagaonkar@slkgroup.com>
2024-03-02 10:34:47 +0530

    Schema service file is created under schemaservice folder

--- src/containers/layout/ui-sections/RightLayout.jsx
+++ src/containers/layout/ui-sections/RightLayout.jsx
@@ -6,7 +6,75 @@ import ButtonStepper from "../../../components/common/ButtonStepper";
 export default function RightLayout() {
   const rightSecSchema = useSelector((state) => state.initialBuData?.rightSection);
   const activeIndex = useSelector((state) => state.initialBuData?.activeIndex);
+const data = [
+  {
+    id: 1,
 
+    fields: [
+      {
+        column: 12,
+        label: "LABEL_TEXT",
+        labelClass: "app-content-label",
+        name: "LABEL_TEXT",
+        showLabel: false,
+        subText: "",
+        tableClass: "",
+        text: "Measurment Type",
+        textClass: "ddl-typography--h5",
+        title: "this is title",
+        type: "LABEL_TEXT",
+        textStyles: { textAlign: "center" }
+      },
+      {
+        column:12,
+        type: "TILE_THUMBNAIL",
+
+        name: "measurmenttype",
+
+        label: "Test Thumbnail",
+
+        labelClass: "app-content-label",
+
+        description: "here is a toggle button",
+
+        attribute_code_value: {
+          defaultIds: [],
+
+          defaultDirection: "tile"
+        },
+
+        options: [
+          {
+            id: 1,
+
+            title: "Flow",
+
+            description:
+              "Flow Measurement is the process of measuring fluid in your plant or industry. You can measure flow through a variety of different devices such as Coriolis, Differential Pressure, Vortex, Magnetic, Ultrasonic, Turbine and positive displacement meters.",
+
+            imgUrl: "https://emerson-cdn.azurewebsites.net/7bd555544cf68071bafa.png"
+          },
+
+          {
+            id: 2,
+
+            title: "Density- Viscosity",
+
+            description:
+              "Our density & viscosity measurement devices offer unbeatableperformance for applications in alcohol concentration, API gravity, specific gravity and more.",
+            imgUrl: ""
+          }
+        ],
+
+        required: true,
+
+        error: "",
+
+        errorClass: ""
+      }
+    ]
+  }
+];
   if (rightSecSchema?.length > 0) {
     const { componentProps } = rightSecSchema[0];
     const activeIndxSchema = getSchemaForDynamicForm(activeIndex, componentProps?.schema);
@@ -15,7 +83,7 @@ export default function RightLayout() {
     return (
       <div>
         <DynamicForm
-          schema={activeIndxSchema}
+          schema={data}
           handleChange={(a, b, c, d) => {
             console.log("Events are trigerring");
             console.log(a, b, c, d);
