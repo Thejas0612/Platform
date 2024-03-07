38a91c8ca5584aa5b284ebe06b8bbb06c809ce5e
Josh Gossett <josh.gossett@emerson.com>
2024-03-07 09:58:44 -0600

    Update description.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { menuData } from "./containers/landing/landingPageCards";
 import { Header } from "./components/dynamic-ui/uiComponentsConfig";
 import Landing from "./containers/landing/Landing";
 import "@emerson/dynamic-ui-public/dist/emerson-ui.css";
@@ -16,9 +15,8 @@ const App = () => {
   return (
     <Router>
       <Header
-        menuData={menuData}
         isAuthenticated={false}
-        title="MSOL PA PLATFORM"
+        title="MSOL Product Advisor"
         logo="https://www.emerson.com/resource/blob/emerson-logo-compressed--data-5576584.png"
       />
       <Switch>
