083e848883ab8bf6bbaaad29e45c988fac6401cf
Dylan Estrada <dylan.estrada@emerson.com>
2024-03-22 16:14:45 -0600

    Styles moved

--- src/components/dropdown-group/DropdownMenu.tsx
+++ src/components/dropdown-group/DropdownMenu.tsx
@@ -2,6 +2,17 @@ import React, { useState } from "react";
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
+const selectStyles = {
+  boxShadow: "none",
+  ".MuiOutlinedInput-notchedOutline": { borderBottom: 0, borderRight: 0, borderLeft: 0 },
+  "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+    border: 0
+  },
+  "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    border: 0
+  }
+}
+
 export interface Option {
   value: string;
   label: string;
@@ -16,7 +27,6 @@ interface DropdownProps {
 
 const DropdownMenu: React.FC<DropdownProps> = ({ id, label, options, onChange }) => {
   const [value, setValue] = useState("");
-
   const handleChange = (event: SelectChangeEvent<unknown>) => {
     const selectedValue = event.target.value as string;
     setValue(selectedValue);
@@ -28,16 +38,7 @@ const DropdownMenu: React.FC<DropdownProps> = ({ id, label, options, onChange })
       <InputLabel shrink={isValueSelected}>{label}</InputLabel>
       <Select
         onChange={handleChange}
-        sx={{
-          boxShadow: "none",
-          ".MuiOutlinedInput-notchedOutline": { borderBottom: 0, borderRight: 0, borderLeft: 0 },
-          "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
-            border: 0
-          },
-          "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-            border: 0
-          }
-        }}
+        sx={selectStyles}
         IconComponent={KeyboardArrowDownIcon}
         value={value}
         label={label}
