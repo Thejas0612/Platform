10a1417abb8135fbd0a33c5218ee3e7a8dbcb005
BSudhaB <sudha.balabhadra@emerson.com>
2024-04-02 17:09:23 +0530

    Added schema in tempPlayground and passing as props and addresed PR comments

--- src/pages/TemperaturePlayground.tsx
+++ src/pages/TemperaturePlayground.tsx
@@ -235,18 +235,108 @@ const schema_Checkmark_dash = {
 const schema_Horizontal_Line = {
    "horizontalLine":true,
 }
+const schema_chart_Data = {
+    "options":  {
+
+      plugins: {
+        "legend": {
+          "display": true,
+          "position": "bottom",
+          "align": "center"
+        },
+        title: {
+          display: true,
+          text: '248R CVD',
+          font: {
+            size: 16,
+            family: 'sans-serif',
+            weight: '10',
+            style: 'bold'
+          },
+          padding: {
+            bottom: 10
+          }
+        },
+      },
+   
+      scales: {
+        x: {
+          title: {
+            display: false,
+            text: 'Ambient Temperature (°C)',
+   
+          },
+          display: true,
+          position: 'center',
+          ticks: {
+            beginAtZero: false,
+            padding: 20,
+            align: 'center', // Center the x-axis labels
+          },
+   
+        },
+        y: {
+          title: {
+            display: false,
+            text: 'Total Probable Error (°C)'
+          },
+          position: "center",
+          min: -3.5000,
+          max: 3.5000,
+        },
+   
+      },
+    },
+  
+"data":[
+   {
+      labels: [-40,-30,-20,10,0,10,20,30,40],
+   
+      datasets: [
+        {
+          position: "bottom",
+          label: '248H HART(tpe -ve)',
+          data: [-2.14,-1.97,-1.8,-1.6,-1.59,-1.53,-1.51,-1.53,-1.59],
+          fill: false,
+          borderColor: 'blue',
+          cubicInterpolationMode: 'monotone',
+        },
+        {
+          label: '248H CVD(tpes -ve)',
+          data: [-1.68,-1.44,-1.25,-1.06,-0.89,-0.78,-0.74,-0.78,-0.89],
+          fill: false,
+          borderColor: 'orange',
+          cubicInterpolationMode: 'monotone',
+        },
+        {
+          label: '248H HART(tpe +ve)',
+          data: [2.14,1.97,1.81,1.69,1.59,1.53,1.51,1.53,1.59],
+          fill: false,
+          borderColor: 'blue',
+          cubicInterpolationMode: 'monotone',
+        },
+   
+        {
+          label: '248H CVD(tpes +ve)',
+          data: [1.68,1.46,1.25,1.06,0.89,0.78,0.74,0.78,0.89],
+          fill: false,
+          borderColor: 'orange',
+          cubicInterpolationMode: 'monotone',
+        },
+      ],
+    }
+]
+};
 
 
 
 const TemperaturePlayground = () => (
    <>
+      <div style={{width:"55%"}}><LineChart schema={schema_chart_Data} /></div>
       <div style={{ width: "40%" }}><TableInput schema={schema} /></div>
       <TileAndThumbnail schema={schema_Tile_Thumbnail} />
       <HorizontalLine  schema={schema_Horizontal_Line}/>
       <Checkmark schema={schema_Checkmark_dash} />
-      <div style={{width:"55%"}}>
-         <LineChart/>
-         </div>
    </>
 )
 
