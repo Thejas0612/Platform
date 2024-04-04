1611b36f5e62d2b868b17b2a0d490962cd1efa32
Ashrith T A <ashrith.t@slkgroup.com>
2024-04-04 11:25:12 +0530

    page refresh issue, left navigation status

--- src/containers/layout/ui-sections/project-lookout/ProjectLookoutRightLayout.jsx
+++ src/containers/layout/ui-sections/project-lookout/ProjectLookoutRightLayout.jsx
@@ -1,16 +1,25 @@
 import { DynamicForm } from "../../../../components/dynamic-ui/uiComponentsConfig";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import ButtonStepperCommon from "../../../../components/button/ButtonStepperCommon";
 import getSchemaForDynamicForm from "../../../../adapterDataManager/schema/getSchema";
+import { updateNavigationStatus } from "./schema-services/schemaMutations";
+import { updateLeftSection } from "../../../../redux/reducers/initialBuDataSlice";
 
 export default function ProjectLookoutRightLayout() {
   const rightSecSchema = useSelector((state) => state.initialBuData?.rightSection);
   const activeIndex = useSelector((state) => state.initialBuData?.activeIndex);
+  const leftSectionSchema = useSelector((state) => state.initialBuData?.leftSection);
+
+  const dispatch = useDispatch();
 
   if (rightSecSchema?.length > 0) {
     const { componentProps } = rightSecSchema[0];
     const activeIndxSchema = getSchemaForDynamicForm(activeIndex, componentProps?.schema);
     const activeIndexCopy = JSON.parse( JSON.stringify( activeIndxSchema ) );
+    const updateSchemaIndex = (screenId) =>{
+      const leftSchema = updateNavigationStatus(leftSectionSchema, screenId);
+      dispatch(updateLeftSection(leftSchema))
+    }
     return (
       <div>
         <DynamicForm
@@ -21,7 +30,7 @@ export default function ProjectLookoutRightLayout() {
           handleKeyPress={(a, b, c) => console.log("a,b,c handleKeyPress", a, b, c)}
           updateData={(a, b, c, d) => console.log("a,b,c,d updateData", a, b, c, d)}/>
         <div>
-          <ButtonStepperCommon />
+          <ButtonStepperCommon updateSchemaIndex={updateSchemaIndex} />
         </div>
       </div>
     );
---
+++ src/containers/layout/ui-sections/project-lookout/schema-services/schemaMutations.js
@@ -0,0 +1,12 @@
+export const updateNavigationStatus = (leftSection, activeIndex) => {
+  return [
+    {
+      ...leftSection[0],
+      componentProps: {
+        schema: leftSection[0]?.componentProps?.schema.map((field) => {
+          return { ...field, selected: field?.ne_id === activeIndex }
+        })
+      }
+    }
+  ];
+};
