fcdaa45e0a37c9520cc5bc08abe8e8164e69c585
T A, Ashrith <Ashrith.TA@emerson.com>
2024-04-09 14:44:55 +0000

    Merged PR 147563: save state between screens
    save state between screens , min,max,normal for flowrate
    
    Related work items: #1575063

--- src/containers/layout/ui-sections/project-lookout/ProjectLookoutRightLayout.jsx
+++ src/containers/layout/ui-sections/project-lookout/ProjectLookoutRightLayout.jsx
@@ -2,8 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import ButtonStepperCommon from "../../../../components/button/ButtonStepperCommon";
 import getSchemaForDynamicForm from "../../../../adapterDataManager/schema/getSchema";
 import MSOLDynamicForm from "../../../../components/shared/dynamicform";
-import { updateNavigationStatus } from "./schema-services/schemaMutations";
-import { updateLeftSection } from "../../../../redux/reducers/initialBuDataSlice";
+import { updateNavigationStatus, saveValuesInSchema } from "./schema-services/schemaMutations";
+import { updateLeftSection, updateRightSection } from "../../../../redux/reducers/initialBuDataSlice";
 
 export default function ProjectLookoutRightLayout() {
   const rightSecSchema = useSelector((state) => state.initialBuData?.rightSection);
@@ -16,16 +16,24 @@ export default function ProjectLookoutRightLayout() {
     const { componentProps } = rightSecSchema[0];
     const activeIndxSchema = getSchemaForDynamicForm(activeIndex, componentProps?.schema);
     const activeIndexCopy = JSON.parse( JSON.stringify( activeIndxSchema ) );
+
     const updateSchemaIndex = (screenId) =>{
       const leftSchema = updateNavigationStatus(leftSectionSchema, screenId);
       dispatch(updateLeftSection(leftSchema))
     }
+
+    const updateValuesInSchema = (formData) =>{
+      const result = saveValuesInSchema(formData, rightSecSchema, activeIndex);
+      dispatch(updateRightSection(result));
+    }
+
     return (
       <div>
         <MSOLDynamicForm
           schema={activeIndexCopy}
           handleChange={(e, formObj, formData, name, isValid) => {
             console.log("e,formObj,formData, name, isValid: ",e, formObj, formData, name, isValid)
+            updateValuesInSchema(formData);
           }}
           handleKeyPress={(a, b, c) => console.log("a,b,c handleKeyPress", a, b, c)}
           updateData={(a, b, c, d) => console.log("a,b,c,d updateData", a, b, c, d)}/>
