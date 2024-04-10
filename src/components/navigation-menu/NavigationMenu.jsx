091fc0c803b6d8705f0a906ea9ea735445dc36d0
sachin.thakur <sachin.thakur@emerson.com>
2024-04-10 17:46:39 +0530

    Removed package, added css modules, other comments

--- src/components/navigation-menu/NavigationMenu.jsx
+++ src/components/navigation-menu/NavigationMenu.jsx
@@ -1,15 +1,15 @@
-import "./navigation-menu.scss";
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import styles from "./NavigationMenu.module.scss";
+import { ArrowForwardIos } from '@mui/icons-material';
 
 const NavigationMenu = ({data, onLabelClick = () => {}}) => {
 
     return (
         <div className={"left-nav"}>
             {data.map(menu => (
-            <div className={"msol_navigation_item " + ((menu.selected) ? "selected" : "")} onClick={() => onLabelClick(menu)}>
+            <div className={styles.msol_navigation_item + " " + ((menu.selected) ? styles.selected : "")} onClick={() => onLabelClick(menu)}>
                 <div className="label">
                     {menu.label}
-                    <ArrowForwardIosIcon className="msol_navigation_item_arrow"/>
+                    <ArrowForwardIos className={styles.arrow}/>
                 </div>
             </div>
             ))}
--- src/containers/layout/ui-sections/temperature/TempLeftLayout.jsx
+++ src/containers/layout/ui-sections/temperature/TempLeftLayout.jsx
@@ -4,8 +4,6 @@ export default function TempLeftLayout({changeIndex}) {
   const leftSection = useSelector((state) => state.initialBuData?.leftSection);
   const activeIndex = useSelector((state) => state.initialBuData?.activeIndex);
   
-  const { componentProps } = leftSection[0];
-
   const allowGoingBack = (menu) => {
     if(menu.step_order < activeIndex) {
       changeIndex(menu.step_order);
@@ -16,7 +14,7 @@ export default function TempLeftLayout({changeIndex}) {
     (leftSection?.length > 0) ?
       <div className="left-nav">
         <NavigationMenu
-          data={componentProps?.schema}
+          data={leftSection[0]?.componentProps?.schema}
           onLabelClick={allowGoingBack}
         />
       </div>
