4f6be70d31cfe2832acde332366439cc03ab9c7a
Josh Gossett <josh.gossett@emerson.com>
2024-04-11 13:22:26 +0000

    Merged PR 153351: Demo

--- src/containers/layout/ui-sections/project-lookout/ProjectLookoutUiLayout.jsx
+++ src/containers/layout/ui-sections/project-lookout/ProjectLookoutUiLayout.jsx
@@ -24,6 +24,8 @@ export default function ProjectLookoutUiLayout() {
       <Grid container>
         <div className="top_section">
           <TopLayout />
+
+        {/*  change */}
         </div>
         <Grid className="left_section" container direction="row">
           <Grid item xs={12} md={3}>
