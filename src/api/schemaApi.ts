a6b5ca29fe01a242fc83956739f76d7e85299f7c
Rajeshwari P Mulemani <rajeshwari.mulemani@slkgroup.com>
2024-04-11 15:04:28 +0530

    changes added to readme file

--- src/api/schemaApi.ts
+++ src/api/schemaApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function findSchemaByBusinessUnitCode(businessUnitCode: string): Promise<object[]> {
     const response = await axios.post<object[]>(
-        `${process.env.REACT_APP_API_URL}/api/schema/loadSchema`,
+        `${import.meta.env.VITE_API_URL}/api/schema/loadSchema`,
         {bu_code: businessUnitCode}
     )
 
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import getEnvModule from "./env";
 import dotenv from "dotenv";
 dotenv.config();
- 
-// getEnvModule().setEnvironmentVarsFromTestEnv(__dirname);
- 
+
 process.env.CLIENT_ID = process.env.SPA_CLIENT_ID || process.env.CLIENT_ID;
  
-// const env = {};
-// [
-//   // List of environment variables made available to the app
-//   ("ISSUER", "CLIENT_ID")
-// ].forEach((key) => {
-//   if (!process.env[key]) {
-//     throw new Error(`Environment variable ${key} must be set. See README.md`);
-//   }
-//   env[key] = process.env[key];
-// });
- 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env": process.env
   },
   resolve: {
-    // alias: {
-    //   '@okta/okta-auth-js': path.resolve(__dirname, 'node_modules/@okta/okta-auth-js/dist/okta-auth-js.umd.js'),
-    //   'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom')
-    // }
+  
   },
   server: {
     port: process.env.PORT || 3000
