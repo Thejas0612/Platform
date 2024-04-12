898545a8ceb8156fc7435035439d6852112aaa3f
Rajeshwari P Mulemani <rajeshwari.mulemani@slkgroup.com>
2024-04-12 20:57:55 +0530

    integrated save sizing API

---
+++ src/components/dialog/alertDialog.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function AlertDialog(props) {
+
+  return (
+    <React.Fragment>
+      <Dialog
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {props.title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+           {props.message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.handleClose}>Close</Button>
+          <Button onClick={props.handleClose} autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
--- src/containers/layout/ui-sections/TopLayout.jsx
+++ src/containers/layout/ui-sections/TopLayout.jsx
@@ -4,8 +4,24 @@ import {
   LabelText,
   ButtonInput
 } from "../../../components/dynamic-ui/uiComponentsConfig";
+import AlertDialog from "../../../components/dialog/alertDialog";
 import "./TopLayout.css";
+import saveSizingApi from "./saveSizingApi";
 export default function TopLayout() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const saveSizing = async () => {
+    let data = await saveSizingApi();
+    handleClickOpen()
+    console.log(data)
+  };
   return (
     <div>
       <div className="SizingSelection">
@@ -45,12 +61,20 @@ export default function TopLayout() {
               btnType="secondary"
               customClass=""
               label="Save Sizing"
-              onClick={() => {}}
+              onClick={() => saveSizing()}
             />
             <ButtonInput btnType="secondary" customClass="" label="Clear" onClick={() => {}} />
+            <AlertDialog
+            message={'Saved Successfully'}
+            open={open}
+            handleClose={handleClose}
+            handleClickOpen={handleClickOpen}
+          />
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+
