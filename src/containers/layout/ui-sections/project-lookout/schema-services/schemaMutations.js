69e5e0d770e78c2a98284e226dc06105229c9e51
Dylan Estrada <dylan.estrada@emerson.com>
2024-04-12 21:40:12 +0000

    Merged PR 153626: Changes to support fluid properties depending on fluid source
    Changes to support fluid properties depending on fluid source![FluidProperties.png](https://dev.azure.com/EmersonAutomationSolutions/8b36abe5-4368-4cdd-836c-147189b59d40/_apis/git/repositories/bb533d8e-e09f-4a72-b0e9-61a4ab3a9f6a/pullRequests/153626/attachments/FluidProperties.png)
    
    Related work items: #1575086

--- src/containers/layout/ui-sections/project-lookout/schema-services/schemaMutations.js
+++ src/containers/layout/ui-sections/project-lookout/schema-services/schemaMutations.js
@@ -46,7 +46,51 @@ export const saveValuesInSchema = (formData, rightSectionSchema, activeIndex) =>
               })
             };
           } else {
-            return screens;
+            if (screens.id === 2 && activeIndex === 1) {
+              const result = screens.fields.map((elementField, key) => {
+                if (elementField.name === "TABLE_INPUT2") {
+                  let data;
+                  const { fields } = formData[0];
+                  const fluidSource = fields.filter((el) => el.name === "fluidSource");
+                  data = elementField?.data.map((row) => {
+                    const currentRow = row.map((formElement) => {
+                      if (fluidSource[0].value === "0") {
+                        if (formElement.type === "TEXT_INPUT" || formElement.name === "density_options") {
+                          return { ...formElement, disabled: true, required: false };
+                        } else {
+                          return formElement;
+                        }
+                      } else {
+                        if (formElement.type === "TEXT_INPUT" || formElement.name === "density_options") {
+                          return {
+                            ...formElement,
+                            disabled: false,
+                            required:
+                              formElement.name === "density_norm" || formElement.name === "viscosity_norm"
+                                ? true
+                                : false
+                          };
+                        } else {
+                          return formElement;
+                        }
+                      }
+                    });
+                    return currentRow;
+                  });
+
+                  elementField = { ...elementField, data: data };
+                  return elementField;
+                } else {
+                  return elementField;
+                }
+              });
+              return {
+                id: screens.id,
+                fields: result
+              };
+            } else {
+              return screens;
+            }
           }
         })
       }
