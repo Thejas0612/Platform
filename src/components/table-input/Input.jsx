cf6a5a95e208ac6caa85838423a6082ff3025515
Dylan Estrada <dylan.estrada@emerson.com>
2024-04-14 22:38:44 +0000

    Merged PR 156265: Update for error messages and input field to get them disabled
    ![image.png](https://dev.azure.com/EmersonAutomationSolutions/8b36abe5-4368-4cdd-836c-147189b59d40/_apis/git/repositories/bb533d8e-e09f-4a72-b0e9-61a4ab3a9f6a/pullRequests/156265/attachments/image.png)

--- src/components/table-input/Input.jsx
+++ src/components/table-input/Input.jsx
@@ -12,34 +12,55 @@ const InputProps = {
 const Input = (props) => {
   const [defaultValue, setDefaultvalue] = useState("");
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('')
+
   const handleChange = (e) => {
     const value = e.target.value;
     const isValid = value.match(TEN_THOUSAND_PLACE_DECIMAL_MATCHER);
 
+    const isFlowRow = ["flow_rate_min", "flow_rate_norm", "flow_rate_max"];
+    const isDensityRow = ["density_min", "density_norm", "density_max"];
+
+    const isViscosityRow = [];
+
     if (isValid) {
       setDefaultvalue(value);
-      if (value < 0) {
+
+      if (isFlowRow.includes(props.schemaProps.name) && value < 0) {
         setError(true);
-      } else if (value > 0 || value === "") {
+        setErrorMessage(
+          "Entered " +
+            props.schemaProps.name.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase()) +
+            " is below 0"
+        );
+      } else if (isDensityRow.includes(props.schemaProps.name) && value <= 0) {
+        setError(true);
+        setErrorMessage(
+          "Entered " +
+            props.schemaProps.name.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase()) +
+            " is below or equal to 0"
+        );
+      } else if (value >= 0 || value === "") {
         setError(false);
+        setErrorMessage("");
       }
     }
   };
   return (
-    <CustomTooltip
-      title={error ? "Entered " + props.schemaProps.name.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase()) + " is below 0" : ""}
-      placement="top"
-    >
+    <CustomTooltip title={error ? errorMessage : ""} placement="top">
       <TextField
         value={defaultValue}
         onChange={handleChange}
-        onBlur={e => setDefaultvalue( defaultValue !== "" ? String(Number(defaultValue).toFixed(4)) : ""  )}
+        onBlur={(e) =>
+          setDefaultvalue(defaultValue !== "" ? String(Number(defaultValue).toFixed(4)) : "")
+        }
         size={props.size}
         error={error}
         InputProps={{
           ...InputProps,
           className: props.schemaProps.required ? "Mui-focused " : ""
         }}
+        disabled={props.schemaProps.disabled}
       />
     </CustomTooltip>
   );
