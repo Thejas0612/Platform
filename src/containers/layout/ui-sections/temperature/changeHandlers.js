4665fe1519f75592d8ba4b4f900e741756021120
Rawat <ayush.rawat@emerson.com>
2024-04-15 20:43:27 +0530

    Adding functionality to Tile & Thumbnail

--- src/containers/layout/ui-sections/temperature/changeHandlers.js
+++ src/containers/layout/ui-sections/temperature/changeHandlers.js
@@ -1,11 +1,12 @@
 import { updateVariation } from "../../../../redux/reducers/initialBuDataSlice";
 
 export const changeHandler = (props) => {
-    const { dispatch, rightSecSchema, name } = props;
+    const { dispatch, rightSecSchema, name, formData } = props;
+    const type = formData[0].fields[0].value;
     switch(name) {
         case "measurmenttype": 
 
-            dispatch(updateVariation({rightSection: rightSecSchema, type: "transmitter"}));
+            dispatch(updateVariation({rightSection: rightSecSchema, type: type}));
             break;
         default:
             console.warn(`changeHandler is not applied for ${name}`);
