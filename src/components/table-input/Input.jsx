b9e8249267a917a451ee051dca9649e4cdc10885
Dylan Estrada <dylan.estrada@emerson.com>
2024-04-15 14:40:49 +0000

    Merged PR 156921: Reverting validations for density and viscosity
    revert validations for density and viscosity

--- src/components/table-input/Input.jsx
+++ src/components/table-input/Input.jsx
@@ -12,50 +12,22 @@ const InputProps = {
 const Input = (props) => {
   const [defaultValue, setDefaultvalue] = useState("");
   const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('')
 
   const handleChange = (e) => {
     const value = e.target.value;
     const isValid = value.match(TEN_THOUSAND_PLACE_DECIMAL_MATCHER);
 
-    const isFlowRow = ["flow_rate_min", "flow_rate_norm", "flow_rate_max"];
-    const isDensityRow = ["density_min", "density_norm", "density_max"];
-
-    const isViscosityRow = ["viscosity_min", "viscosity_norm", "viscosity_max"];
-
     if (isValid) {
       setDefaultvalue(value);
-
-      if (isFlowRow.includes(props.schemaProps.name) && value < 0) {
+      if (value < 0) {
         setError(true);
-        setErrorMessage(
-          "Entered " +
-            props.schemaProps.name.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase()) +
-            " is below 0"
-        );
-      } else if (isDensityRow.includes(props.schemaProps.name) && value <= 0) {
-        setError(true);
-        setErrorMessage(
-          "Entered " +
-            props.schemaProps.name.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase()) +
-            " is below or equal to 0"
-        );
-      } else if (isViscosityRow.includes(props.schemaProps.name) && value < 0.01) {
-        setError(true);
-        setErrorMessage(
-          "Entered " +
-          props.schemaProps.name.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase()) +
-          " is below or equal to 0.01"
-        );
-      }
-      else if (value >= 0 || value === "") {
+      } else if (value > 0 || value === "") {
         setError(false);
-        setErrorMessage("");
       }
     }
   };
   return (
-    <CustomTooltip title={error ? errorMessage : ""} placement="top">
+    <CustomTooltip title={error ? "Entered " + props.schemaProps.name.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase()) + " is below 0" : ""} placement="top">
       <TextField
         value={defaultValue}
         onChange={handleChange}
