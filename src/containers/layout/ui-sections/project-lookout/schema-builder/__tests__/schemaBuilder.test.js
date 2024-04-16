51a62befc47e265127e8b42cc534c5c85f74d992
Dylan Estrada <dylan.estrada@emerson.com>
2024-04-16 20:55:48 +0000

    Merged PR 158012: updating the schema builder test to include table input builder
    updating the schema builder test to include table input builder.

--- src/containers/layout/ui-sections/project-lookout/schema-builder/__tests__/schemaBuilder.test.js
+++ src/containers/layout/ui-sections/project-lookout/schema-builder/__tests__/schemaBuilder.test.js
@@ -10,15 +10,33 @@ describe("schemaBuilder", () => {
       .screen(0)
       .tileThumbnail("measurement-type")
       .onChange((field) => {
-        const viscosityOption = field.data?.find(_ => _.id === VISCOSITY_ID);
+        const viscosityOption = field.data?.find((_) => _.id === VISCOSITY_ID);
         if (viscosityOption == null) {
           throw new Error("Could not find viscosityOption.");
         }
 
-        viscosityOption.disabled = true
+        viscosityOption.disabled = true;
       })
       .build(0, screenSchemaNew);
 
+    expect(schema).toMatchSnapshot();
+  });
+
+  it("when select specific gravity and then elements disappear", () => {
+    const schema = schemaBuilder(oldSchema)
+      .screen(2)
+      .tableInput("TABLE_INPUT2")
+      .onChange((field, value) => {
+        //doing changes for specific_gravity
+        field.data[1][1] = {};
+        field.data[1][3] = {
+          type: "TEXT",
+          label: "@60F and 14.7psia",
+          align: "center"
+        };
+        field.data[1][4] = {};
+      })
+      .build(0, screenSchemaNew);
     expect(schema).toMatchSnapshot();
   });
 });
