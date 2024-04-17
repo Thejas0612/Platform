403b105ea5d4a096e792aa23672a1dd25ddd17d6
sachin.thakur <sachin.thakur@emerson.com>
2024-04-17 17:22:02 +0530

    getFieldComponentByName

--- src/containers/layout/ui-sections/schemaMutations.jsx
+++ src/containers/layout/ui-sections/schemaMutations.jsx
@@ -1,23 +1,31 @@
 
 export const changeStepperIndex = (leftSection, step) => {
-    if (!leftSection || !leftSection.length || !leftSection[0]?.componentProps?.schema) {
-      console.warn('Invalid leftSection argument provided to changeStepperIndex.');
-      return leftSection;
-    }
-  
-    const schema = leftSection[0].componentProps.schema;
-  
-    const updatedSchema = schema.map((option, i) => ({
-      ...option,
-      selected: option.ne_id === step,
-    }));
-  
-    return [{
-      ...leftSection[0],
-      componentProps: {
-        ...leftSection[0].componentProps,
-        schema: updatedSchema,
-      },
-    }];
-  };
-  
+  if (!leftSection || !leftSection.length || !leftSection[0]?.componentProps?.schema) {
+    console.warn('Invalid leftSection argument provided to changeStepperIndex.');
+    return leftSection;
+  }
+
+  const schema = leftSection[0].componentProps.schema;
+
+  const updatedSchema = schema.map((option, i) => ({
+    ...option,
+    selected: option.ne_id === step,
+  }));
+
+  return [{
+    ...leftSection[0],
+    componentProps: {
+      ...leftSection[0].componentProps,
+      schema: updatedSchema,
+    },
+  }];
+};
+
+export const getFieldComponentByName = (name, activeIndex, schema) => {
+  const section = schema.find(section => section.id === activeIndex);
+  if (section && section.fields) {
+    const field = section.fields.find(field => field.name === name);
+    return field || null;
+  }
+  return null;
+}
