8367af3dd70241d82e3fa84520add8800d8c9ae4
Josh Gossett <josh.gossett@emerson.com>
2024-04-17 18:44:01 +0000

    Merged PR 157928: Simplified ThumbnailOrTile types.
    Just some clean up.

--- src/components/shared/msol-tile-or-thumbnail/MsolTileOrThumbnail.tsx
+++ src/components/shared/msol-tile-or-thumbnail/MsolTileOrThumbnail.tsx
@@ -17,7 +17,7 @@ export interface MsolTileOrThumbnailProps {
   othAttr?: { type: string };
   name: string;
   hideCheckboxes?: boolean;
-  singleSelect?:boolean;
+  singleSelect?: boolean;
 }
 
 export const MsolTileOrThumbnail: FC<MsolTileOrThumbnailProps> = ({
@@ -30,8 +30,8 @@ export const MsolTileOrThumbnail: FC<MsolTileOrThumbnailProps> = ({
                                                                     },
                                                                     othAttr,
                                                                     name,
-                                                                  hideCheckboxes = false,
-                                                                  singleSelect = false
+                                                                    hideCheckboxes = false,
+                                                                    singleSelect = false
                                                                   }) => {
   // TODO: This is temporary to support backwards compatibility. This will be removed in the future.
   const dataOverride = data ? data : options;
@@ -47,25 +47,25 @@ export const MsolTileOrThumbnail: FC<MsolTileOrThumbnailProps> = ({
       <Stack spacing={4}>
         {
           dataOverride.map(item => {
-            let checked: boolean;
-            if(singleSelect){
-              checked = (value === item.id) ? true : false;
+            let checked = false;
+            if (singleSelect) {
+              checked = value === item.id;
             } else {
-              checked = value && value.includes(item.id) ? true : false
+              checked = value != null && value.includes(item.id);
             }
 
             const handleChange: MouseEventHandler<HTMLDivElement> = (event) => {
-                const newChecked : boolean = !checked;
-                let newValue;
-                if(singleSelect) {
-                  newValue = newChecked ? item.id : null;
-                } else {
-                  newValue = newChecked ? [...value as string[], item.id] : (value as string[]).filter(id => id !== item.id);
-                }
-                setValue(newValue);
-                onChange(event, othAttr?.type, name, newValue);
+              const newChecked = !checked;
+              let newValue;
+              if (singleSelect) {
+                newValue = newChecked ? item.id : null;
+              } else {
+                newValue = newChecked ? [...value as string[], item.id] : (value as string[]).filter(id => id !== item.id);
+              }
+              setValue(newValue);
+              onChange(event, othAttr?.type, name, newValue);
             };
-            
+
             return <MsolTitleOrThumbnailItem key={item.id}
                                              onChange={handleChange}
                                              checked={checked}
