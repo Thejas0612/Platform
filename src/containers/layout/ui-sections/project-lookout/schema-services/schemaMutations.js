0f5d3459cec507ff33ee2db35747d6c5c64d3dbf
Dylan Estrada <dylan.estrada@emerson.com>
2024-04-29 13:06:41 -0600

    label alignment and also bug fixed for custom scenarios

--- src/containers/layout/ui-sections/project-lookout/schema-services/schemaMutations.js
+++ src/containers/layout/ui-sections/project-lookout/schema-services/schemaMutations.js
@@ -19,55 +19,8 @@ export const saveValuesInSchema = (formData, rightSectionSchema, activeIndex) =>
       ...rightSectionSchema[0],
       componentProps: {
         schema: rightSectionSchema[0]?.componentProps?.schema.map((screens) => {
-          if (screens.id == activeIndex) {
             return screens
-          } else {
-            if (screens.id === 2 && activeIndex === 1) {
-              const result = screens.fields.map((elementField, key) => {
-                if (elementField.name === "TABLE_INPUT2") {
-                  let data;
-                  const { fields } = formData[0];
-                  const fluidSource = fields.filter((el) => el.name === "fluid-source");
-                  data = elementField?.data.map((row) => {
-                    const currentRow = row.map((formElement) => {
-                      if (fluidSource[0].value === FLUID_SOURCE_OPTIONS.DATABASE) {
-                        if (formElement.type === "TEXT_INPUT" || formElement.name === "density_options") {
-                          return { ...formElement, disabled: true, required: false };
-                        } else {
-                          return formElement;
-                        }
-                      } else {
-                        if (formElement.type === "TEXT_INPUT" || formElement.name === "density_options") {
-                          return {
-                            ...formElement,
-                            disabled: false,
-                            required:
-                              formElement.name === "density_norm" || formElement.name === "viscosity_norm"
-                                ? true
-                                : false
-                          };
-                        } else {
-                          return formElement;
-                        }
-                      }
-                    });
-                    return currentRow;
-                  });
-
-                  elementField = { ...elementField, data: data };
-                  return elementField;
-                } else {
-                  return elementField;
-                }
-              });
-              return {
-                id: screens.id,
-                fields: result
-              };
-            } else {
-              return screens;
-            }
-          }
+         
         })
       }
     }
