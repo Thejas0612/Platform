c4679f66eba163289e51967a2291df2329538eae
Lohith-kumar-gangadevi <lohithkumar.gangadevi@emerson.com>
2024-05-02 16:01:03 +0530

    WIP: schema based target validation

--- src/containers/layout/custom/CustomLayout.jsx
+++ src/containers/layout/custom/CustomLayout.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import MSOLDynamicForm from "../../../components/shared/dynamicform";
 import { Grid } from "@mui/material";
+import { generateFields } from "../../../utils/validation.service";
 
 function CustomLayout() {
   const [schema, setSchema] = React.useState([
@@ -35,8 +36,7 @@ function CustomLayout() {
           disabled: false,
           value: "",
           options: [],
-          required: true,
-          error: "required",
+          error: "",
           errorClass: "",
           placeholder: "Pick One",
           isApiOnEvent: {
@@ -50,9 +50,10 @@ function CustomLayout() {
             targetProperty: "options"
           },
           dataSourceUrl:
-            "https://webapp-z-autosol-msolst-n-001.azurewebsites.net/api/processcondition/processPipingLineSize?buCode=dpflow",
+            "https://webapp-z-autosol-msolst-n-001.azurewebsites.net/api/processcondition/processPipingPipeSchedule?buCode=dpflow&lineSize=",
           display: true,
-          order: 1
+          order: 1,
+          required: true
         },
         {
           column: 3,
@@ -83,6 +84,7 @@ function CustomLayout() {
           display: false,
           order: 3,
           validations: {
+            required: true,
             minValue: 1,
             maxValue: 100
           }
@@ -99,200 +101,421 @@ function CustomLayout() {
             { id: "Inch", label: "Inch", value: "Inch" },
             { id: "mm", label: "mm", value: "mm" }
           ],
-          required: true,
+          required: false,
           error: "",
           errorClass: "",
           display: false,
           order: 4
         },
         {
-          column: 12,
-          label: "LABEL_TEXT",
-          labelClass: "app-content-label",
-          name: "process_fluid",
-          showLabel: false,
-          subText: "",
-          tableClass: "",
-          text: "Process Fluid",
-          textClass: "ddl-typography--h5",
-          title: "this is title",
-          type: "LABEL_TEXT",
-          textStyles: {
-            textAlign: "start"
-          },
-          display: true,
-          order: 5
-        },
-        {
-          type: "CUSTOM_BUTTON_GROUP",
-          column: 12,
-          title: "Fluid Type",
-          label: "Fluid Type",
-          name: "fluidtype",
-          data: [
+          "id": "fluidType",
+          "type": "CUSTOM_BUTTON_GROUP",
+          "column": 12,
+          "label": "Fluid Type",
+          "name": "fluidtype",
+          "defaultId": "1",
+          "data": [
             {
-              id: "0",
-              label: "Liquid"
+              "id": "1",
+              "label": "Liquid"
             },
             {
-              id: "1",
-              label: "Gas"
+              "id": "2",
+              "label": "Gas"
             },
             {
-              id: "2",
-              label: "Steam"
+              "id": "3",
+              "label": "Steam"
             },
             {
-              id: "3",
-              label: "Natural Gas"
+              "id": "4",
+              "label": "Natural Gas"
             },
             {
-              id: "4",
-              label: "Slurry"
+              "id": "5",
+              "label": "Slurry"
             }
           ],
-          multiple: true,
-          defaultIds: ["0"],
-          showLabel: true,
-          wrapperId: "",
-          wrapperClass: "cutsom_class",
-          labelClass: "ddl-from-custom-label",
-          value: "0",
-          attribute_code_value: {
-            multiple: true,
-            size: "medium"
-          },
-          required: true,
-          display: true,
-          order: 6
+          "labelClass": "app-content-label camelCase"
+        },
+        {
+          "id": "liquidfluidSource",
+          "column": 2,
+          "type": "RADIO_INPUT",
+          "name": "fluidSource",
+          "label": "Fluid Source",
+          "labelClass": "app-content-label camelCase",
+          "value": "0",
+          "options": [
+            {
+              "value": "0",
+              "label": "Database"
+            },
+            {
+              "value": "1",
+              "label": "Custom"
+            }
+          ],
+          "error": "",
+          "errorClass": "",
+          "visibility": [
+            {
+              "id": "fluidType",
+              "value": "1"
+            }
+          ]
         },
         {
           column: 3,
-          type: "RADIO_INPUT",
-          name: "fluidsource",
-          label: "Fluid Source",
-          labelClass: "app-content-label",
-          selectedIds: ["0"],
-          disabledIds: ["1"],
-          options: [
-            {
-              value: "0",
-              label: "Database",
-              id: "0"
-            },
-            {
-              value: "1",
-              label: "Custom",
-              id: "1"
-            }
-          ],
-          value: "0",
-          required: true,
-          error: "",
-          errorClass: "",
-          display: true,
-          order: 7
-        },
-        {
-          column: 4,
           type: "SINGLE_SELECT",
-          name: "fluidsdatabase",
-          label: "Fluids Database",
+          name: "pipeschedule",
+          label: "Liquid Fluid Database",
           labelClass: "app-content-label",
+          disabled: false,
           value: "",
           options: [],
           required: true,
           error: "",
           errorClass: "",
-          placeholder: "Pick One",
-          dataSourceUrl:
-            "https://webapp-z-autosol-msolst-n-001.azurewebsites.net/api/processcondition/fluidsDatabase?buCode=dpflow&fluidType=LIQUID",
           display: true,
-          order: 8
-        },
-        {
-          column: 12,
-          align: "left",
-          btnType: "textary",
-          error: "",
-          errorClass: "",
-          label: "Additional Options",
-          required: true,
-          showBackIcon: false,
-          type: "BUTTON",
-          value: "",
-          display: true,
-          order: 9,
-          name: "additional_options_label"
-        },
-        {
-          column: 12,
-          disabledIds: [],
-          error: "",
-          errorClass: "",
-          labelClass: "app-content-label",
-          name: "additional_options",
-          options: [
+          order: 2,
+          visibility: [
             {
-              label:
-                "Fluid Plugs or Clogs (High Viscosity, Slurry, Entrained Solids, Solidifies, Etc)",
-              value: "0"
+              "id": "liquidfluidSource",
+                "value": "0"
             },
             {
-              label: "Fluid Causes Wear and Erosion ( Entrained Solids, Abrasive, Etc.)",
-              value: "1"
+              "id": "fluidType",
+              "value": "1"
+            }
+          ]
+        },
+        {
+          "label": "Enter Custom Fluid Name",
+          "labelClass": "app-content-label camelCase",
+          "type": "TEXT_INPUT",
+          "name": "fluidsDatabaseInput",
+          "column": 4,
+          "value": "",
+          "visibility": [
+            {
+              "id": "liquidfluidSource",
+              "value": "1"
+            },
+            {
+              "id": "fluidType",
+              "value": "1"
+            }
+          ]
+        },
+        {
+          "id": "gasfluidSource",
+          "column": 2,
+          "type": "RADIO_INPUT",
+          "name": "fluidSource",
+          "label": "Fluid Source",
+          "labelClass": "app-content-label camelCase",
+          "value": "0",
+          "options": [
+            {
+              "value": "0",
+              "label": "Database"
+            },
+            {
+              "value": "1",
+              "label": "Custom"
             }
           ],
+          "error": "",
+          "errorClass": "",
+          "visibility": [
+            {
+              "id": "fluidType",
+              "value": "2"
+            }
+          ]
+        },
+        {
+          column: 3,
+          type: "SINGLE_SELECT",
+          name: "pipeschedule",
+          label: "Gas Fluid Database",
+          labelClass: "app-content-label",
+          disabled: false,
+          value: "",
+          options: [],
           required: true,
-          selectedIds: ["1"],
-          showAlert: false,
-          type: "CHECKBOX_INPUT",
+          error: "",
+          errorClass: "",
           display: true,
-          order: 10
-        }
+          order: 2,
+          visibility: [
+            {
+              "id": "gasfluidSource",
+              "value": "0"
+            },
+            {
+              "id": "fluidType",
+              "value": "2"
+            }
+          ]
+        },
+        {
+          "label": "Enter Gas Custom Fluid Name",
+          "labelClass": "app-content-label camelCase",
+          "type": "TEXT_INPUT",
+          "name": "fluidsDatabaseInput",
+          "column": 4,
+          "value": "",
+          "visibility": [
+            {
+              "id": "gasfluidSource",
+              "value": "1"
+            },
+            {
+              "id": "fluidType",
+              "value": "2"
+            }
+          ]
+        },
+        {
+          "id": "liquidfluidSource",
+          "column": 2,
+          "type": "RADIO_INPUT",
+          "name": "fluidSource",
+          "label": "Fluid Source",
+          "labelClass": "app-content-label camelCase",
+          "value": "1",
+          "options": [
+            {
+              "value": "0",
+              "label": "Database"
+            },
+            {
+              "value": "1",
+              "label": "Custom"
+            }
+          ],
+          "error": "",
+          "errorClass": "",
+          "visibility": [
+            {
+              "id": "fluidType",
+              "value": "5"
+            }
+          ]
+        },
+        {
+          column: 3,
+          type: "SINGLE_SELECT",
+          name: "pipeschedule",
+          label: "Slurry Fluid Database",
+          labelClass: "app-content-label",
+          disabled: true,
+          value: "",
+          options: [],
+          required: true,
+          error: "",
+          errorClass: "",
+          display: true,
+          order: 2,
+          visibility: [
+            {
+              "id": "liquidfluidSource",
+              "value": "1"
+            },
+            {
+              "id": "fluidType",
+              "value": "5"
+            }
+          ]
+        },
+        {
+          "label": "Enter Custom Fluid Name",
+          "labelClass": "app-content-label camelCase",
+          "type": "TEXT_INPUT",
+          "name": "fluidsDatabaseInput",
+          "column": 4,
+          "value": "",
+          "visibility": [
+            {
+              "id": "liquidfluidSource",
+              "value": "0"
+            },
+            {
+              "id": "fluidType",
+              "value": "5"
+            }
+          ]
+        },
+        // {
+        //   column: 12,
+        //   label: "LABEL_TEXT",
+        //   labelClass: "app-content-label",
+        //   name: "process_fluid",
+        //   showLabel: false,
+        //   subText: "",
+        //   tableClass: "",
+        //   text: "Process Fluid",
+        //   textClass: "ddl-typography--h5",
+        //   title: "this is title",
+        //   type: "LABEL_TEXT",
+        //   textStyles: {
+        //     textAlign: "start"
+        //   },
+        //   display: true,
+        //   order: 5
+        // },
+        // {
+        //   type: "CUSTOM_BUTTON_GROUP",
+        //   column: 12,
+        //   title: "Fluid Type",
+        //   label: "Fluid Type",
+        //   name: "fluidtype",
+        //   data: [
+        //     {
+        //       id: "0",
+        //       label: "Liquid"
+        //     },
+        //     {
+        //       id: "1",
+        //       label: "Gas"
+        //     },
+        //     {
+        //       id: "2",
+        //       label: "Steam"
+        //     },
+        //     {
+        //       id: "3",
+        //       label: "Natural Gas"
+        //     },
+        //     {
+        //       id: "4",
+        //       label: "Slurry"
+        //     }
+        //   ],
+        //   multiple: true,
+        //   defaultIds: ["0"],
+        //   showLabel: true,
+        //   wrapperId: "",
+        //   wrapperClass: "cutsom_class",
+        //   labelClass: "ddl-from-custom-label",
+        //   value: "0",
+        //   attribute_code_value: {
+        //     multiple: true,
+        //     size: "medium"
+        //   },
+        //   required: true,
+        //   display: true,
+        //   order: 6
+        // },
+        // {
+        //   column: 3,
+        //   type: "RADIO_INPUT",
+        //   name: "fluidsource",
+        //   label: "Fluid Source",
+        //   labelClass: "app-content-label",
+        //   selectedIds: ["0"],
+        //   disabledIds: ["1"],
+        //   options: [
+        //     {
+        //       value: "0",
+        //       label: "Database",
+        //       id: "0"
+        //     },
+        //     {
+        //       value: "1",
+        //       label: "Custom",
+        //       id: "1"
+        //     }
+        //   ],
+        //   value: "0",
+        //   required: true,
+        //   error: "",
+        //   errorClass: "",
+        //   display: true,
+        //   order: 7
+        // },
+        // {
+        //   column: 4,
+        //   type: "SINGLE_SELECT",
+        //   name: "fluidsdatabase",
+        //   label: "Fluids Database",
+        //   labelClass: "app-content-label",
+        //   value: "",
+        //   options: [],
+        //   required: true,
+        //   error: "",
+        //   errorClass: "",
+        //   placeholder: "Pick One",
+        //   dataSourceUrl:
+        //     "https://webapp-z-autosol-msolst-n-001.azurewebsites.net/api/processcondition/fluidsDatabase?buCode=dpflow&fluidType=LIQUID",
+        //   display: true,
+        //   order: 8
+        // },
+        // {
+        //   column: 12,
+        //   align: "left",
+        //   btnType: "textary",
+        //   error: "",
+        //   errorClass: "",
+        //   label: "Additional Options",
+        //   required: true,
+        //   showBackIcon: false,
+        //   type: "BUTTON",
+        //   value: "",
+        //   display: true,
+        //   order: 9,
+        //   name: "additional_options_label"
+        // },
+        // {
+        //   column: 12,
+        //   disabledIds: [],
+        //   error: "",
+        //   errorClass: "",
+        //   labelClass: "app-content-label",
+        //   name: "additional_options",
+        //   options: [
+        //     {
+        //       label:
+        //         "Fluid Plugs or Clogs (High Viscosity, Slurry, Entrained Solids, Solidifies, Etc)",
+        //       value: "0"
+        //     },
+        //     {
+        //       label: "Fluid Causes Wear and Erosion ( Entrained Solids, Abrasive, Etc.)",
+        //       value: "1"
+        //     }
+        //   ],
+        //   required: true,
+        //   selectedIds: ["1"],
+        //   showAlert: false,
+        //   type: "CHECKBOX_INPUT",
+        //   display: true,
+        //   order: 10
+        // }
       ]
     }
   ]);
 
-  //create a function to loop throug the above schema array
+  const handleInput = React.useCallback((e, formData, schemaData, fieldName) => {
+    setSchema(schemaData)
+  }, []);
 
-  const generateFeilds = (inputFeilds) => {
-    if (!inputFeilds.length) {
-      return [];
-    }
-    return inputFeilds;
-    //find the datasource url and make the api call
-  };
-
-  const handleInput = (e, formData, schemaData, fieldName) => {
-    // const newSchema = schemaData.slice();
-    // const latestSchema = newSchema.map((group, i) => {
-    //   group.fields.map(field => {
-    //     if (field.id === "email") {
-    //       field.value = "hello@gmail.com"
-    //     }
-    //     return field
-    //   })
-    //   return group
-    // })
-    // console.log(latestSchema);
-    // setSchema(JSON.parse(JSON.stringify(latestSchema)))
-    // const currentFeild = schema.reduce((prev, curr, i) => {
-    //     const field = prev || curr.fields.find(field => field.name === fieldName);
-    //     if(field !== 'undefined') return field
-    // }, undefined)
-  };
 
   return (
     <>
       <Grid container>
         <Grid item xs={6}>
           <MSOLDynamicForm
-            schema={schema}
+            schema={[
+              {
+                fields: generateFields(schema[0].fields)
+              }
+            ]}
             handleChange={(event, a, b, c) => handleInput(event, a, b, c)}
-            handleKeyPress={function noRefCheck() {}}
-            handleSubmit={function noRefCheck() {}}
-            updateData={function noRefCheck() {}}
+            handleKeyPress={function noRefCheck() { }}
+            handleSubmit={function noRefCheck() { }}
+            updateData={function noRefCheck() { }}
           />
         </Grid>
       </Grid>
